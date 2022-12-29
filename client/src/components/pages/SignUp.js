3b3f007235f3ec23f2d6d301bd2143a8429e0b0f
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-28 23:19:18 -0500

    Home,products, signup js , css

---
+++ client/src/components/pages/SignUp.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import '../pages/SignUp.css';
+import { Login } from "../Login";
+import { Register } from '../Register';
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className='App'>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
